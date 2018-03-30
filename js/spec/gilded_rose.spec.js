51aaf29aa3c9ea4cfc95682e7999e9e2b5ed7b5f
venaud <34676647+venaud@users.noreply.github.com>
2018-03-30 17:23:25 +0200

    Add fixtures inside js tests

--- js/spec/gilded_rose.spec.js
+++ js/spec/gilded_rose.spec.js
@@ -1,19 +1,31 @@
-'use strict';
+'use strict'
 
-const expect = require('chai').expect;
-const {Item, Shop} = require('../src/gilded_rose');
+const expect = require('chai').expect
+const {Item, Shop} = require('../src/gilded_rose')
 
-describe("Gilded Rose", function() {
+describe('Gilded Rose', function () {
+    const items = []
+    items.push(new Item('+5 Dexterity Vest', 10, 20))
+    items.push(new Item('Aged Brie', 2, 0))
+    items.push(new Item('Elixir of the Mongoose', 5, 7))
+    items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80))
+    items.push(new Item('Sulfuras, Hand of Ragnaros', -1, 80))
+    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20))
+    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49))
+    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49))
+    // this conjured item does not work properly yet
+    items.push(new Item('Conjured Mana Cake', 3, 6))
 
-  it("should foo", function() {
-    // Given
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    it('should foo', function () {
+        // Given
+        const items = [new Item('foo', 0, 0)]
+        const gildedRose = new Shop(items)
 
-    // When
-    const items = gildedRose.updateQuality();
+        // When
+        const updatedItems = gildedRose.updateQuality()
 
-    // Then
-    expect(items[0].name).to.equal("fixme");
-  });
+        // Then
+        expect(items[0].name).to.equal('fixme')
+    })
 
-});
+})
--- js/spec/gilded_rose.test.js
+++ js/spec/gilded_rose.test.js
@@ -1,18 +1,30 @@
-'use strict';
+'use strict'
 
-const {Item, Shop} = require('../src/gilded_rose');
+const {Item, Shop} = require('../src/gilded_rose')
 
-describe("Gilded Rose", function() {
+describe('Gilded Rose', function () {
 
-  it("should foo", function() {
-    // Given
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const items = []
+    items.push(new Item('+5 Dexterity Vest', 10, 20))
+    items.push(new Item('Aged Brie', 2, 0))
+    items.push(new Item('Elixir of the Mongoose', 5, 7))
+    items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80))
+    items.push(new Item('Sulfuras, Hand of Ragnaros', -1, 80))
+    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20))
+    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49))
+    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49))
+    // this conjured item does not work properly yet
+    items.push(new Item('Conjured Mana Cake', 3, 6))
 
-    // When
-    const items = gildedRose.updateQuality();
+    it('should foo', function () {
+        // Given
+        const gildedRose = new Shop([new Item('foo', 0, 0)])
 
-    // Then
-    expect(items[0].name).toEqual("fixme");
-  });
+        // When
+        const items = gildedRose.updateQuality()
 
-});
+        // Then
+        expect(items[0].name).toEqual('fixme')
+    })
+
+})
